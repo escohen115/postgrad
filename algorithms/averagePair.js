44511354e9106375debdc5fe44e523e6ddf17528
simon <escohen115@gmail.com>
2021-05-14 09:49:48 -0400

    a

---
+++ algorithms/averagePair.js
@@ -0,0 +1,19 @@
+
+function averagePair(arr, target){
+    let left = 0
+    let right = arr.length-1
+    while (left<right){
+        if (arr[left]+arr[right]/2==target){
+            return true
+        }
+        if (arr[left]+arr[right]/2>target){
+            right--
+        }
+        if (arr[left]+arr[right]/2<target){
+            left++
+        }
+    }
+    return false 
+}
+
+averagePair([1,2,3],2.5)
