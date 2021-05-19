8467a130d87403f68829820e252e80bd0a369b32
simon <escohen115@gmail.com>
2021-05-18 21:05:06 -0400

    a

---
+++ algorithms/minSubarraySum.js
@@ -0,0 +1,25 @@
+function minSubArrayLen(arr, sum) {
+    let total = 0
+    let start = 0
+    let end = 0
+    let minLen = Infinity
+
+    while (start < arr.length){
+
+       if (total < sum && end < arr.length){
+           total+=arr[end]
+           end++
+       }
+       else if (total >= sum){
+           minLen = Math.min(minLen, end-start)
+           total -= arr[start]
+           start++
+       }
+       else {
+           break
+       }
+    }
+    return minLen === Infinity ? 0 : minLen
+}
+
+minSubArrayLen([2,3,1,2,4,3], 7)
