06589d67027b98de0fe760ef1ad4ba2ac108d2a4
simon <escohen115@gmail.com>
2021-05-24 16:09:36 -0400

    a

--- algorithms/getDigit.js
+++ algorithms/getDigit.js
@@ -1,7 +1,5 @@
 function getDigit (num, place){
     let index = num.toString().length - place -1
-    console.log(index)
     let str = num.toString()
-    console.log(str)
     return parseInt(str[index])
 }
