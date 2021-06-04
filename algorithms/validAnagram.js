9f061f06306d2009e1bafa80973d78c899385e1f
simon <escohen115@gmail.com>
2021-06-04 12:37:12 -0400

    a

---
+++ algorithms/validAnagram.js
@@ -0,0 +1,27 @@
+function validAnagram (str1, str2){
+    if (str1.length !== str2.length){
+        return false
+    }
+    let freq1 = {}
+    let freq2 = {}
+    for (let char of str1){
+        freq1[char] = ++freq1[char] || 1
+    }
+    for (let char of str2){
+        freq2[char] = ++freq2[char] || 1
+    }
+//     console.log(freq1)
+//     console.log(freq2)
+    for (let key in freq1){
+        if (!(key in freq2)){
+            return false
+        }
+        if (freq1[key]!==freq2[key]){
+            return false
+        }
+    }
+    return true
+    
+}
+
+validAnagram("texttwisttime","timetwisttext")
