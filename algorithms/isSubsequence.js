709fef46fd9eb00d00f8275e112ae90e8e15afbd
simon <escohen115@gmail.com>
2021-06-16 10:49:21 -0400

    a

--- algorithms/isSubsequence.js
+++ algorithms/isSubsequence.js
@@ -2,7 +2,7 @@ function isSubsequence (str1, str2){
     let i = 0
     let j = 0
 
-    while (j<str2.length){
+    while (j < str2.length){
         if(str2[j]===str1[i]){
             i++
         }
@@ -14,7 +14,6 @@ function isSubsequence (str1, str2){
 }
     
 
-// isSubsequence('hello', 'hello world')
 isSubsequence('acb', 'abc')
 // isSubsequence('abc', 'abracadabra')
 
