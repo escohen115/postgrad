a2513b4499c8d5fa28b5c9ac1d0769d7e04194fd
simon <escohen115@gmail.com>
2021-06-16 10:49:05 -0400

    a

--- algorithms/frequencyCounter.js
+++ algorithms/frequencyCounter.js
@@ -2,11 +2,14 @@ function same (arr1, arr2){
     if (arr1.length !== arr2.length){
         return false
     }
+
     let freq1 = {}
     let freq2 = {}
+
     for (let val of arr1){
         freq1[val] = ++freq1[val] || 1
     }
+
     for (let val of arr2){
         freq2[val] = ++freq2[val] || 1
     }
@@ -21,8 +24,5 @@ function same (arr1, arr2){
     }
     return true
 }
-
-
-
 
 same([1,6,3,6],[36,36,9,2])
