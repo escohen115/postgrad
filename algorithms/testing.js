e00107865e45c36cd18d6c5a1dc717301f471ef8
simon <escohen115@gmail.com>
2021-06-22 13:21:50 -0400

    a

--- algorithms/testing.js
+++ algorithms/testing.js
@@ -1,6 +1,6 @@
 
 function findMinAndRemove(firstHalf, secondHalf){
- let minfirstHalf = firstHalf[0]
+let minfirstHalf = firstHalf[0]
 let minsecondHalf = secondHalf[0]
 
 if(minfirstHalf < minsecondHalf){
