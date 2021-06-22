cb67b3a49ab65713af617bbecd591d0568052fe3
simon <escohen115@gmail.com>
2021-06-22 13:21:35 -0400

    a

--- algorithms/validAnagram.js
+++ algorithms/validAnagram.js
@@ -4,12 +4,14 @@ function validAnagram (str1, str2){
     }
     let freq1 = {}
     let freq2 = {}
+
     for (let char of str1){
         freq1[char] = ++freq1[char] || 1
     }
     for (let char of str2){
         freq2[char] = ++freq2[char] || 1
     }
+    
     for (let key in freq1){
         if (!(key in freq2)){
             return false
