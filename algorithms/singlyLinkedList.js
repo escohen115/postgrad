762e6b27392acd4ff0d68db8943af2893cdc6aaf
simon <escohen115@gmail.com>
2021-06-23 17:44:56 -0400

    a

--- algorithms/singlyLinkedList.js
+++ algorithms/singlyLinkedList.js
@@ -11,13 +11,14 @@ class SinglyLinkedList {
         this.tail = null;
         this.length = 0;
     }
-    
+
     push(val) {
         var newNode = new Node(val);
         if (!this.head) {
             this.head = newNode;
             this.tail = this.head;
-        } else {
+        } 
+        else {
             this.tail.next = newNode;
             this.tail = newNode;
         }
