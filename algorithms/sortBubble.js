922e1f9b89e77124f68c4d66a161a18b8097578a
simon <escohen115@gmail.com>
2021-06-28 20:44:49 -0400

    a

--- algorithms/sortBubble.js
+++ algorithms/sortBubble.js
@@ -1,8 +1,8 @@
 function bubbleSort(arr){
     for (let i = arr.length; i > 0; i--){
         let noSwaps = true
-        for (let j=0; j < i-1; j++){
-            if (arr[j]>arr[j+1]){
+        for (let j = 0; j < i-1; j++){
+            if (arr[j] > arr[j+1]){
                 let temp = arr[j]
                 arr[j] = arr[j+1]
                 arr[j+1] = temp
