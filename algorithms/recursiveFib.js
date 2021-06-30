15c7ebf9d3f0f05d5fc8cbc8b1dcffb267df66dd
simon <escohen115@gmail.com>
2021-06-30 10:57:39 -0400

    a

--- algorithms/recursiveFib.js
+++ algorithms/recursiveFib.js
@@ -1,6 +1,6 @@
 function recursiveFib(num){
     if (num <= 2) return 1
-    return recursiveFib(num-1) + recursiveFib(num-2)
+    return recursiveFib(num - 1) + recursiveFib(num - 2)
 }
 
 recursiveFib(6)
