8f1c552828aae411192151e0a036fb38de24291b
simon <escohen115@gmail.com>
2021-07-22 10:53:17 -0400

    a

--- algorithms/findMostRepeated.js
+++ algorithms/findMostRepeated.js
@@ -2,11 +2,12 @@ function fmr(n){
     let answer = null
     let hash = {}
     
-    for(let i of n){
+    for (let i of n){
         hash[i] = ++hash[i] || 1
     }
 
     let freq = Math.max(...Object.values(hash))
+
     Object.keys(hash).forEach(function(key){
         if (hash[key]===freq){
             answer = key
@@ -16,3 +17,24 @@ function fmr(n){
 }
 
 fmr([3,4,4,4,4,4,6,6,6,1,1,4,4,6,6,6,6,6,6,6,6,6,6,6,6,4,4,4])
+
+
+
+function fmr (arr) {
+
+    let freq = {}
+    let max = -Infinity
+    let answer = null
+
+    for (let i of arr){
+        freq[i] ? freq[i] += 1 : freq[i] = 1
+    }
+
+    for (let i in freq){
+        if (freq[i] > max ){
+            max = freq[i]
+            answer = i
+        }
+    }
+    return parseInt(answer)
+}
