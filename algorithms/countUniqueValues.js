ceb972ecfd88900a81afcd46d5ec7230e91f5245
escohen115 <escohen115@gmail.com>
2021-08-24 11:48:31 -0400

    a

--- algorithms/countUniqueValues.js
+++ algorithms/countUniqueValues.js
@@ -14,3 +14,11 @@ function countUniqueValues(arr){
     return arr.slice(0,i)
 }  
 countUniqueValues([1,1,2,3,4,4,5,5,5,5,6,7,8,9])
+
+// 8/23/21
+function duplicates (arr){
+    let set = new Set(arr)
+    if (set.size !== arr.length) return true
+    return false
+}
+
