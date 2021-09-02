4618069120c3bb21e8cf166bb86c565f26d05cb0
simon <escohen115@gmail.com>
2021-09-02 14:48:35 -0400

    a

---
+++ algorithms/linkedListPartition.js
@@ -0,0 +1,20 @@
+// untested
+let partition = (list, k) => {
+    let list1 = null
+    let list2 = null
+
+    let elem = list.head
+
+    while (elem.next !== null){
+        if (elem < k){
+            list1.push(elem)
+        }
+        else{
+            list2.push(elem)
+        }
+        elem = elem.next
+    }
+
+    list1.tail.next = list2.head
+    
+}
