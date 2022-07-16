97b6858cee637a5a2bf9e6a8f7a98563f925d9a5
simon <escohen115@gmail.com>
2022-07-16 12:07:48 -0400

    a

--- algorithms/sortQuick.js
+++ algorithms/sortQuick.js
@@ -1,126 +1,58 @@
-function pivot (arr, comp){
-    let count = 0
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] < arr[comp]){
-            count++
-            swap(arr, i, count)   
-        }
-    }
-    swap(arr, comp, count )
-    console.log(arr)
-    return count
-}
 
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
 
-function swap (arr, i1, i2){
-    let temp = arr[i1]
-    arr[i1] = arr[i2]
-    arr[i2] = temp
-    return arr
-}
-
-
-function sort(arr){
-    for (let i = 0; i < arr.length; i++){
-        pivot(arr, i)
-    }
-    return arr
-}
-
-
-pivot ([3,1,2,4], 0)
-
-// bad stuff ->
-
-
-function pivotHelper(arr){
-    let pivotIndex = Math.floor(arr.length / 2)
-    let comp = arr[pivotIndex]
-    let lessThan = 0
-
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] < comp){
-            lessThan ++
-        }    
-    }
-    let temp = arr[lessThan]
-    arr[lessThan] = arr[pivotIndex]
-    arr[pivotIndex] = temp
-    return pivotIndex
-}
-
-
-function quickSort(arr){
-    for (let i = 0; i < arr.length; i++){
-        pivotHelper(arr)
-    }
-    return arr
-}
-
-quickSort([0,9,7,8,34,3,2,4,1,5])
-
-
-//good stuff ->
-
-function partition(arr, start = 0, end = arr.length - 1) {
-  function swap (arr, a, b){
-     let temp = arr[a]
-     arr[a] = arr[b]
-     arr[b] = temp
-  }
-
-  // We are assuming the partition is always the first element
-  let partition = arr[start];
+  // We are assuming the pivot is always the first element
+  let pivot = arr[start];
   let swapIdx = start;
 
-  for (let i = start + 1; i <= end; i++) {
-    if (arr[i] < partition) {
+  for (let i = start + 1; i <= end; i++) { //compare each element with the "pivot", in this case the first array element
+    if (arr[i] < pivot) { //track how many values are less than it, so we can swap the pivot to be the last of them
       swapIdx++;
       swap(arr, swapIdx, i);
     }
   }
 
-  // Swap the partition from the start the swapPoint
+  // Swap the pivot from the start the swapPoint
   swap(arr, start, swapIdx);
   return swapIdx;
 }
 
 
 function quickSort(arr, left = 0, right = arr.length -1){
-    if (left < right){
-        let partitionIndex = partition(arr, left, right) //3
+    if(left < right){
+        let pivotIndex = pivot(arr, left, right) //3
         //left
-        quickSort(arr,left,partitionIndex-1);
+        quickSort(arr,left,pivotIndex-1);
         //right
-        quickSort(arr,partitionIndex+1,right);
+        quickSort(arr,pivotIndex+1,right);
       }
      return arr;
 } 
            
-quickSort([100,-3,2,4,6,9,1,2,5,3,23])
+console.log (quickSort([100,-3,2,4,6,9,1,2,5,3,23]))
 
 
 
-// quick sort pseudocode
-// solved through recursion
-// partition pseudocode
-// iterate through array.length - 1 times
-// take first item of array and move all items that are smaller to the left, larger to the right
-// return index of sorted item
-// 
+
+// [4,6,9,1,2,5,3]
+// first element less than the pivot is 1. increase the swap index from 0 to 1. swap 6 (at the swap index) with 1 (at i)
+// [4,1,9,6,2,5,3]
+// next, 2 is less than 4. increase the swap index from 1 to 2. swap 9 (at the swap index) with 2 (at i)
+// [4,1,2,6,9,5,3]
+// next, 3 is less than 4. increase the swap index from 2 to 3. swap 6 (at the swap index) with 3 (at i)
+// [4,1,2,3,9,5,6]
+// next, now that we've finished our for loop, swap the pivot (found at the start) with the swap index
+// [3,1,2,4,9,5,6]
 
 
-function swap(arr,a,b){
-    let temp = arr[a]
-    arr[a] = arr[b]
-    arr[b] = temp
-}
 
-function partition(arr,start = 0, end = arr.length - 1){
-    let x = arr[start]
-    let index = start
-    for (let i = index+1; i <= end; i++){
-        if (arr[i])
-    }
-    
-}
+// [3,2,1,4,6,9,5]
+//        4
+//  3,2,1    6,9,5
+//      3      6
+//  2,1      5  9
+//    2
+//  1
