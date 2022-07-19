ff177224c1c5bee878e0be62df9462e8e8f96af8
simon <escohen115@gmail.com>
2022-07-19 12:21:39 -0400

    a

--- algorithms/hashTable.js
+++ algorithms/hashTable.js
@@ -6,15 +6,26 @@ class HashTable{
     _hash(key){
         let total = 0
         let prime = 31
-        for (let i=0; i < Math.min(100, key.length;i++)){
+        for (let i=0; i < Math.min(100, key.length);i++){
             let char = key[i]
             let value = char.charCodeAt(0) - 96
-            total = (total * prime + value) % this.key.length
+            total = (total * prime + value) % this.keyMap.length
         }
         return total
     }
     set(key, value){
         let index = this._hash(key)
-        return index
+        if (!this.keyMap[index]){
+            this.keyMap[index] = []
+        }
+        this.keyMap[index].push([key,value])
     }
 }
+
+let ht = new HashTable(4)
+ht.set("pizza","yummy")
+ht.set("icecream","yummy")
+ht.set("steak","yummy")
+ht.set("brussel sprouts","yucky")
+
+console.log(ht.keyMap)
