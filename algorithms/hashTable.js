356e022d7dd4554b10d08953118b8a02eaee92bf
simon <escohen115@gmail.com>
2022-07-19 14:25:02 -0400

    a

--- algorithms/hashTable.js
+++ algorithms/hashTable.js
@@ -1,4 +1,3 @@
-
 class HashTable{
     constructor(size=53){
         this.keyMap = new Array(size);
--- algorithms/mergeSort.js
+++ algorithms/mergeSort.js
@@ -33,4 +33,41 @@ function mergeSort(arr){
 }
 
 // console.log(merge([1,3,5],[2,4,]))
+// console.log(mergeSort([4,3,2,-89]))
+
+
+
+function merge(arr1, arr2){
+    let ma = []
+    let i = 0
+    let j = 0
+    while (i < arr1.length && j < arr2.length){
+        if (arr1[i] < arr2[j]){
+            ma.push(arr1[i])
+            i++
+        }
+        else{
+            ma.push(arr2[j])
+            j++
+        }
+    }
+    if (i < arr1.length){
+        ma.push(...arr1.slice(i))
+    }
+    if (j < arr2.length){
+        ma.push(...arr2.slice(j))
+    }
+    return ma
+}
+// console.log(merge([1,3,5],[2,4,]))
+
+function mergeSort(arr){
+    let mid = Math.floor(arr.length/2)
+
+    if (arr.length <=1){
+        return arr
+    }
+    return merge(mergeSort(arr.slice(0,mid)), mergeSort(arr.slice(mid,arr.length)))
+}
+
 console.log(mergeSort([4,3,2,-89]))
