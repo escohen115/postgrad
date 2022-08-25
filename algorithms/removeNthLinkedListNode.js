aba1f79f9fb3ed4af7322d0f2194e1c16f5e8bc2
Simon Cohen <52968378+escohen115@users.noreply.github.com>
2022-08-25 11:43:24 -0400

    a

---
+++ algorithms/removeNthLinkedListNode.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+function remove (head, n){
+
+    let left = head
+    let right = head
+
+    for (let i=0;i<n+1;i++){
+        right = right.next
+    }
+
+    while(right !== null){
+        right=right.next
+        left = left.next
+    }
+    left.next = left.next.next
+}
