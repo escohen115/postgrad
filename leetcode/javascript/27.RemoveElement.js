4cea04191797762c2678d5dd7421a5f61016ec1a
simon <escohen115@gmail.com>
2022-11-02 09:08:00 -0400

    a

--- leetcode/javascript/27.RemoveElement.js
+++ leetcode/javascript/27.RemoveElement.js
@@ -1,11 +1,18 @@
-var removeElement = function(nums, val) {
-    let total = nums.length
-    for (let i=0;i<nums.length;i++){
-        if(nums[i]===val){
-            nums[i] = Infinity
-            total--
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(arr, num) {
+
+    let counter = 0
+    for (let i = 0;i<arr.length;i++){ 
+        if (arr[i] === num){
+            counter++
+            arr[i] = Infinity
         }
     }
-    nums.sort((a,b)=>a-b)
-    return total
+  arr.sort((a,b)=>a-b)
+  return arr.length - counter
+    
 };
